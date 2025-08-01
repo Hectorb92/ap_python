debd4dede7c8590dc5cadb4621e10842ee5c6103
BOLAGAY CODENA HECTOR JAVIER <hbolagay@sweadenseguros.com>
2025-08-01 10:22:01 -0500

    hbolagay

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-print ("Hola")
-print
+
+# POR TERMINAL
+# python -m pip install flask 
+from flask import Flask, jsonify
+
+app = Flask (__name__)
+
+
+# Ejemplo 1 
+'''
+from productos import productos
+@app.route('/ping')
+def ping():
+    return 'pong' 
+    
+'''
+# Ejemplo 2 
+from productos import productos
+@app.route('/ping')
+def ping():
+    return 
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
---
+++ productos.py
@@ -0,0 +1,6 @@
+productos = [
+
+    { "name": "lapto" , "price":"800" , "cantidad":4},
+    { "name": "mouse" , "price":"30" , "cantidad":8},
+      { "name": "teclado" , "price":"80" , "cantidad":200}
+]
